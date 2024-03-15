0fade6d31145c8bcd315aae149f708ae3a86f678
Camille <dev@camimaki.io>
2024-03-15 14:59:24 +0100

    first commit

---
+++ Sagano/SaganoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaganoApp.swift
+//  Sagano
+//
+//  Created by Camille on 15/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SaganoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
